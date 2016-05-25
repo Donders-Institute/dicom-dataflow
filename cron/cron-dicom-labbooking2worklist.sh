a7063c19944ce7493b947906c6e048082158d320
Hurng-Chun Lee <h.lee@donders.ru.nl>
2016-05-25 10:32:41 +0200

    update with cron job script

---
+++ cron/cron-dicom-labbooking2worklist.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+source /opt/_modules/setup.sh
+module load python
+module load cluster 
+source $CLUSTER_UTIL_ROOT/share/cluster-lib.sh
+cwd=$( get_script_dir $0 )
+
+export DCCN_PYTHONDIR=/home/tg/honlee/projects/hpc-utility/python
+
+WLBROKER_DIR=/scratch/data/OrthancData/DicomWorklist/WLBROKER
+
+#today=$( date +%Y-%m-%d )
+today=2016-05-27
+
+$DCCN_PYTHONDIR/bin/dicom-labbooking2worklist.py -c ${cwd}/config.ini -d $today -s $WLBROKER_DIR
