781bb58e04c09eee487ec1bf4074d6bcb8248768
Hurng-Chun Lee <h.lee@donders.ru.nl>
2016-05-25 10:33:57 +0200

    retrive date of today

--- cron/cron-dicom-labbooking2worklist.sh
+++ cron/cron-dicom-labbooking2worklist.sh
@@ -10,7 +10,7 @@ export DCCN_PYTHONDIR=/home/tg/honlee/projects/hpc-utility/python
 
 WLBROKER_DIR=/scratch/data/OrthancData/DicomWorklist/WLBROKER
 
-#today=$( date +%Y-%m-%d )
-today=2016-05-27
+today=$( date +%Y-%m-%d )
+#today=2016-05-27
 
 $DCCN_PYTHONDIR/bin/dicom-labbooking2worklist.py -c ${cwd}/config.ini -d $today -s $WLBROKER_DIR
