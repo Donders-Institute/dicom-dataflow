3bf6d3141fb01c3d8520b0dea1748e4069b2c3bb
Hurng-Chun Lee <h.lee@donders.ru.nl>
2016-07-01 16:25:59 +0200

    add title into stager task data

--- docker/stager/test/test_insertjob.sh
+++ docker/stager/test/test_insertjob.sh
@@ -2,11 +2,13 @@
 
 src=/project/3010000.03/raw/mri/SUBJ0003/
 dst=$( echo $src | sed 's|/project|irods:/rdm/di/dccn/DAC_3010000.01_173|g' )
+now=$( date )
 
 curl -H "Content-Type: application/json" -X POST -d \
 "{ \"type\": \"rdm\",
    \"data\": { \"srcURL\": \"${src}\",
-               \"dstURL\": \"${dst}\"},
+               \"dstURL\": \"${dst}\",
+               \"title\": \"[${now}] push to ${dst}\"},
    \"options\": { \"attempts\": 3,
                   \"backoff\": { \"delay\": 60000, \"type\": \"fixed\" }}
  }" http://localhost:3000/job
