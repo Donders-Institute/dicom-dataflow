21a8bd1b7e40cd6c6f1af7a87f45c8792d0d7ea5
Hurng-Chun Lee <h.lee@donders.ru.nl>
2016-09-13 21:16:39 +0200

    improve the wrapper of irsync

--- docker/stager/bin/s-irsync.sh
+++ docker/stager/bin/s-irsync.sh
@@ -19,19 +19,37 @@ fi
 src=$( echo $1 | sed 's/irods:/i:/g' )
 dst=$( echo $2 | sed 's/irods:/i:/g' )
 
+w_total=0
+
 ## check source type/existence
 is_src_dir=0
 echo $src | egrep '^i:' > /dev/null 2>&1
 if [ $? -eq 0 ]; then
-    ils $src > /dev/null 2>&1
+    src_coll=$( echo $src | sed 's/^i://' )
+    ils $src_coll > /dev/null 2>&1
     if [ $? -ne 0 ]; then
-        echo "file or collection not found: $src" 1>&2
+        echo "file or collection not found: $src_coll" 1>&2
         exit 1
     fi
+
+    # TODO: find a better way to determine the source is a collection or a data object
+    ils ${src_coll}/ > /dev/null 2>&1
+    if [ $? -eq 0 ]; then
+        is_src_dir=1
+        # determine size of the sync task: number of data objects in the source collection 
+        w_total=$( iquest --no-page "n=%s" "select UNIQUE(DATA_NAME) where COLL_NAME = '${src_coll}'" | grep 'n=' | wc -l )
+        w_total=$(( $w_total + $(iquest --no-page "n=%s" "select UNIQUE(DATA_NAME) where COLL_NAME like '${src_coll}/%'" | grep 'n=' | wc -l) ))
+    else
+        w_total=1
+    fi 
 else
     if [ -e $src ]; then
         if [ -d $src ]; then
             is_src_dir=1
+            # determine size of the sync task: number of files in the directory 
+            w_total=$( find $src -type f | wc -l )
+        else
+            w_total=1
         fi
     else
         echo "file or directory not found: $src" 1>&2
@@ -46,18 +64,26 @@ is_dst_dir=0
 echo $dst | egrep '^i:' > /dev/null 2>&1
 if [ $? -eq 0 ]; then
     is_irods=1
-fi 
-
-echo $dst | egrep '.*/$' > /dev/null 2>&1
-if [ $? -eq 0 ]; then
-    is_dst_dir=1
-fi
-
-if [ $is_dst_dir -eq 1 ]; then
-    if [ $is_irods -eq 1 ]; then
-        imkdir -p "$( echo $dst | sed 's/^i://g')"
+    dst_coll=$( echo $dst | sed 's/^i://' )
+    # TODO: find a better way to determine whether the irods namespace is existing and whether it's a directory
+    ils ${dst_coll} > /dev/null 2>&1
+    if [ $? -eq 0 ]; then
+        ils ${dst_coll}/ > /dev/null 2>&1
+        # this is an existing irods collection
+        if [ $? -eq 0 ]; then
+            is_dst_dir=1
+        fi
     else
-        mkdir -p "${dst}"
+        # the namespace doesn't exist at all, whether it should be a directory is determined by is_src_dir
+        is_dst_dir=$is_src_dir
+    fi
+else
+    if [ -e $dst ]; then
+        if [ -d $dst ]; then
+            is_dst_dir=1
+        fi
+    else
+        is_dst_dir=$is_src_dir
     fi
 fi
 
@@ -66,7 +92,35 @@ if [ $is_src_dir -eq 1 ] && [ $is_dst_dir -ne 1 ] ; then
     exit 1
 fi
 
-if [ $is_src_dir -eq 1 ] || [ $is_dst_dir -eq 1 ] ; then
-    irsync -v -K -r "${src}" "${dst}"
-    exit $?
+# make sure the dst_dir is created
+if [ $is_dst_dir -eq 1 ]; then
+    if [ $is_irods -eq 1 ]; then
+        imkdir -p "$( echo $dst | sed 's/^i://' )" > /dev/null 2>&1
+    else
+        mkdir -p "${dst}"
+    fi
+fi
+
+# reconstruct the dst w/ proper filename
+if [ $is_src_dir -eq 0 ] && [ $is_dst_dir -eq 1 ] ; then
+    fname=$( echo $src | awk -F '/' '{print $NF}' )
+    dst=${dst}/${fname}
+fi
+
+# run irsync 
+if [ $w_total -gt 0 ]; then
+    w_done=0
+    w_done_percent=0
+    irsync -v -K -r "${src}" "${dst}" | while read -r line; do
+        w_done=$(( $w_done + 1 ))
+        w_done_percent_new=$(( $w_done * 100 / $w_total ))
+        if [ $w_done_percent_new -gt $w_done_percent ]; then
+            w_done_percent=$w_done_percent_new
+            echo $w_done_percent_new
+        fi
+    done
+    exit ${PIPESTATUS[0]}
+else
+    echo "nothing to sync" 
+    exit 0
 fi
