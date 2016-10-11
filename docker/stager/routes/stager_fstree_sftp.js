eff79ccfb8919159e5778d5bd438e81443129925
Hurng-Chun Lee <h.lee@donders.ru.nl>
2016-10-11 18:37:25 +0200

    bugfix

--- docker/stager/routes/stager_fstree_sftp.js
+++ docker/stager/routes/stager_fstree_sftp.js
@@ -14,40 +14,46 @@ var _getDirList = function(request, response) {
     var f_data = [];
 
     var c = new Client();
-    var handle_error = function(err) {
+    var handle_error = function(err, code) {
         c.end();
+        console.log(typeof err);
         console.error(err);
         response.contentType('application/json');
-        response.json(f_data);
+        response.status(code);
+        response.end('something wrong');
     };
 
-    try {
-        c.on( 'ready', function() {
-            c.sftp( function(err, sftp) {
-                if (err) throw err;
-
-                sftp.readdir(dir, function(err, list) {
-                    if (err) {
-                        handle_error(err);
-                    } else {
-                        list.forEach(function(f) {
-                            if ( f.longname.substr(0,1) == 'd' ) {
-                                f_data.push( { 'name': f.filename, 'type': 'd', 'size': -1 } );
-                            } else if (f.longname.substr(0,1) != 'l') { // ignore symbolic links
-                                f_data.push( { 'name': f.filename, 'type': 'f', 'size': f.attrs.size } );
-                            }
-                        });
-                        c.end();
-                        response.contentType('application/json');
-                        response.send(f_data);
-                    }
+    if ( typeof dir === 'undefined' || dir == '' ) {
+        handle_error("directory not specified", 404); 
+    } else {
+        try {
+            c.on( 'ready', function() {
+                c.sftp( function(err, sftp) {
+                    if (err) throw err;
+ 
+                    sftp.readdir(dir, function(err, list) {
+                        if (err) {
+                            handle_error(err, 500);
+                        } else {
+                            list.forEach(function(f) {
+                                if ( f.longname.substr(0,1) == 'd' ) {
+                                    f_data.push( { 'name': f.filename, 'type': 'd', 'size': -1 } );
+                                } else if (f.longname.substr(0,1) != 'l') { // ignore symbolic links
+                                    f_data.push( { 'name': f.filename, 'type': 'f', 'size': f.attrs.size } );
+                                }
+                            });
+                            c.end();
+                            response.contentType('application/json');
+                            response.send(f_data);
+                        }
+                    });
                 });
-            });
-        }).on( 'error', function(err) {
-            handle_error(err);
-        }).connect(cfg);
-    } catch(err) {
-        handle_error(err);
+            }).on( 'error', function(err) {
+                handle_error(err, 500);
+            }).connect(cfg);
+        } catch(err) {
+            handle_error(err, 500);
+        }
     }
 }
 
