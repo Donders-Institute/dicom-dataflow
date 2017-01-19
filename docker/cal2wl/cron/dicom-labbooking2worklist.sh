838d1786911b50f000663006b6948ab724a92d15
Hurng-Chun Lee <h.lee@donders.ru.nl>
2017-01-19 16:42:45 +0100

    rearrange configuration files into 'config' folders

--- docker/cal2wl/cron/dicom-labbooking2worklist.sh
+++ docker/cal2wl/cron/dicom-labbooking2worklist.sh
@@ -5,4 +5,4 @@ export PATH=$PYTHON_PREFIX/bin:$DCMTK_PREFIX/bin:$PATH
 today=$( date +%Y-%m-%d )
 #today=2016-06-01
 
-$DCCN_PYTHONDIR/bin/dicom-labbooking2worklist.py -c /opt/config.ini -d $today -s $WLBROKER_DIR
+$DCCN_PYTHONDIR/bin/dicom-labbooking2worklist.py -c /opt/config/config.ini -d $today -s $WLBROKER_DIR
